bb97a271e4d271dba50bcc0f82b0405dcaedbde0
alizaynoune <simo.jokir@gmail.com>
2020-12-26 15:45:12 +0100

    test
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,44 @@
 $( document ).ready(function() {
-    $('#page').load('src/Home.html');
+    $('.page').load('src/Home.html');
 });
 
 function    _loop_elem(){
-    var active = $("#topnav").children();
+    var active = $(".menu").children();
     for (var i = 0; i < active.length; i++){
-        $(active[i]).removeClass('active');
+        $(active[i]).removeClass('currpage');
     }
 }
 
 function _onclick(elem){
     _loop_elem();
-    $(elem).addClass('active');
-    var page = $('#page').children();
+    $(elem).addClass('currpage');
+    var page = $('.page').children();
     _load_page(elem);
 }
 
 function        _load_page(elem){
-    $('#page').load($(elem).attr("value"));
+    $('.page').load($(elem).attr("value"));
+}
+
+function        openClose(elem){
+    if ($(elem).attr("value") === "open"){
+        $(elem).attr("value", "close");
+        $(elem).html("&#10005;")
+        openNav();
+    } 
+    else{
+        $(elem).attr("value", "open");
+        $(elem).html("&#9776;");
+        closeNav();
+    }
 }
 
 function openNav() {
-    $("#myNav").css("width","100%");
-    $("#myNav").css("margin-left", "0%");
+    $(".menu").css("width","40%");
+    $(".menu").css("margin-left", "60%");
   }
   
   function closeNav() {
-    $("#myNav").css("width","0%");
-    $("#myNav").css("margin-left", "100%");
+    $(".menu").css("width","0%");
+    $(".menu").css("margin-left", "100%");
   }
