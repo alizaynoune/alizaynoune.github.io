3ff8b5e33a9ddc066bd988ee9b84704cc0670163
alizaynoune <simo.jokir@gmail.com>
2021-01-04 19:32:23 +0100

    add animation
    Signed-off-by: alizaynoune <simo.jokir@gmail.com>

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,8 +1,15 @@
 
 function readFile() {
-    var elem = $('.discription');
-    console.log($(elem[0]));
+    var elem = $('.project', '.all_project');
+    $(elem).each(function(){
+        var thisDiv = $('.hidenDesc', this);
+        jQuery.get($(thisDiv).attr('name'), function(data){
+            data = data.replace(/\n/g, '<br/>');
+            $(thisDiv).append(`<p >${data}</p>`);
+        });
+    });
 }
+
 
 $(document).ready(function () {
         jQuery.get("discription/info.txt", function (data) {
@@ -13,10 +20,9 @@ $(document).ready(function () {
                 var _new = `<div class=\"project\"><h2 class=\"name\">${_file_name[0]}</h2>
             <span onclick=\"toggleDesc(this)\"class=\"_desactive\">descrition</span>
             <a class=\"link_repo\" href=\"${sp_space[1]}\">source code</a>
-            <div class=\"discription$\" name=\"discription/${sp_space[0]}\"></div>
+            <div class=\"hidenDesc\" name=\"discription/${sp_space[0]}\"></div>
             </div>`;
                 $(".all_project").append(_new);
-               // console.log(length);
             }
             readFile();
     });
@@ -25,7 +31,8 @@ $(document).ready(function () {
 
 function toggleDesc(elem) {
     $(elem).parent().toggleClass('show');
-
+    $(elem).next().next().toggleClass('showDesc');
+   
 }
 
 
